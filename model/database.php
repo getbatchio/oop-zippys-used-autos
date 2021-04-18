7a5721bb40754ac5a04cc479663726980b3c4549
getbatchio <75768830+getbatchio@users.noreply.github.com>
2021-04-18 19:13:50 -0400

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,36 +1,42 @@
 <?php
-class Database {
-    private static $host;
-    private static $dbname;
+
+   
+class Database{
+    
+  
+    private static $hostname;
     private static $username;
     private static $password;
-    private static $dsn;
     private static $db;
 
-    public function __construct(){
-        self::$host = getenv('SQL_HOST') ;
-        self::$dbname = getenv('SQL_DB') ;
-        self::$username = getenv('SQL_USER') ;
-        self::$password = getenv('SQL_PW') ;
-        self::$dsn = 'mysql:host=' . self::$host . ';dbname=' . self::$dbname;
-    }
 
-    public static function getDB() {
-        if (!isset(self::$db)) {
-            try {
-                self::$db = new PDO(self::$dsn,
-                                    self::$username,
-                                    self::$password);
-            } catch (PDOException $e) {
-                $error_message = 'Database Error: ';
-                $error_message .= $e->getMessage();
-                include('view/error.php');
-                exit();
-            }
-        }
-        return self::$db;
+    public function __construct(){
+     self::$url = getenv('JAWSDB_URL');
+     self::$dbparts = parse_url($url);
+     self::$dsn = $dbparts['host'];
+     self::$username = $dbparts['user'];
+     self::$password = $dbparts['pass'];
+     self::$database = ltirm($dbparts['path'],'/');
+
     }
+    public static function getDB(){
+        
+      if (!isset(self::$db)){
+                try {
+                    self::$db = new PDO("mysql:host=".self::$hostname.";dbname=".self::$database."",self::$username,self::$password);
+                    //echo 'connection good';
+                } catch (PDOException $e) {
+                    $error =$e->getMessage();
+                    include('view/error.php');
+                    exit();
+                } 
+            }
+    return self::$db;
 }
+
+   
+}
+
 $database = new Database();
 
    
