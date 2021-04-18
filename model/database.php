95ba0aa2f7b67c877c08f413f127942ee561c888
getbatchio <75768830+getbatchio@users.noreply.github.com>
2021-04-18 18:06:26 -0400

    update database

--- model/database.php
+++ model/database.php
@@ -1,27 +1,27 @@
 <?php
-
-   
+  
 class Database{
   
-    private static $dsn = $dbparts['host'];
-    private static $username = $dbparts['user'];
-    private static $password = $dbparts['pass'];
+    private static $host;
+    private static $dbname;
+    private static $username;
+    private static $password;
+    private static $dsn;
     private static $db;
 
-
-    
-    
-
-
     public function __construct(){
-     
+        self::$host = getenv('SQL_HOST');
+        self::$dbname = getenv('SQL_DB');
+        self::$username = getenv('SQL_USER');
+        self::$password = getenv('SQL_PW');
+        self::$dsn = 'mysql:host=' . self::$host . ';dbname=' . self::$dbname;
     }
     public static function getDB(){
         
       if (!isset(self::$db)){
                 try {
                     self::$db = new PDO(self::$dsn,self::$username,self::$password);
-                    //echo 'connection good';
+    
                 } catch (PDOException $e) {
                     $error =$e->getMessage();
                     include('view/error.php');
