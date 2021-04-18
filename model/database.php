69620d6fc76e77150be7034aaf0882505b240bde
getbatchio <75768830+getbatchio@users.noreply.github.com>
2021-04-18 18:50:20 -0400

    update database

--- model/database.php
+++ model/database.php
@@ -1,48 +1,35 @@
 <?php
-
-   
-class Database{
-    private static $url;
-    private static $dbparts;
-    private static $hostname;
+class Database {
+    private static $host;
+    private static $dbname;
     private static $username;
     private static $password;
-    private static $database;
     private static $dsn;
     private static $db;
-    
-
-
-    
-    
-
 
     public function __construct(){
-     self::$url = getenv('JAWSDB_URL');
-     self::$dbparts = parse_url($url);
-     self::$dsn = $dbparts['host'];
-     self::$username = $dbparts['user'];
-     self::$password = $dbparts['pass'];
-     self::$database = ltirm($dbparts['path'],'/');
-     self::$dsn = 'mysql:host=' . self::$hostname . ';dbname=' . self::$database;
-
+        self::$host = getenv('SQL_HOST') ;
+        self::$dbname = getenv('SQL_DB') ;
+        self::$username = getenv('SQL_USER') ;
+        self::$password = getenv('SQL_PW') ;
+        self::$dsn = 'mysql:host=' . self::$host . ';dbname=' . self::$dbname;
     }
-    public static function getDB(){
-        
-      if (!isset(self::$db)){
-                try {
-                    self::$db = new PDO(self::$dsn,self::$username,self::$password);
-                    
-                } catch (PDOException $e) {
-                    $error =$e->getMessage();
-                    include('view/error.php');
-                    exit();
-                } 
-            }
-    return self::$db;
-}
 
-   
+    public static function getDB() {
+        if (!isset(self::$db)) {
+            try {
+                self::$db = new PDO(self::$dsn,
+                                    self::$username,
+                                    self::$password);
+            } catch (PDOException $e) {
+                $error_message = 'Database Error: ';
+                $error_message .= $e->getMessage();
+                include('view/error.php');
+                exit();
+            }
+        }
+        return self::$db;
+    }
 }
 $database = new Database();
 
